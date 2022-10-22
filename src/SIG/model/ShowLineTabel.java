f3eb81ce17b6afb547f8617ea58ed65d1d7fe075
islamgbr <islamkhaled51@gmail.com>
2022-10-22 23:10:12 +0200

    sales_invoice_generator

---
+++ src/SIG/model/ShowLineTabel.java
@@ -0,0 +1,67 @@
+
+
+//model of item lines table 
+
+package SIG.model;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author  Islam Gbr
+ */
+public class ShowLineTabel extends AbstractTableModel {
+
+    private final String[] colums = {"Num", "Item", "Unit Price", "Quantity", "Total"};
+    private final ArrayList<sigItem> items;
+
+    public ShowLineTabel(ArrayList<sigItem> items) {
+        this.items = items;
+    }
+
+    @Override
+    public int getRowCount() {
+        return items.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colums.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        sigItem item = items.get(rowIndex);
+        switch(columnIndex){
+            case 0 -> {
+                return item.getInvoice().getNum();
+            }
+                
+            case 1 -> {
+                return item.getItemName();
+            }
+                
+            case 2 -> {
+                return item.getUnitPrice();
+            }
+                
+            case 3 -> { 
+                return item.getQuantity();
+            }
+            
+            case 4 -> { 
+                return item.getTotalLine();
+            }
+            
+        }
+        
+        return null;
+
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return colums[column];
+    }
+}
